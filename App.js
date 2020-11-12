77f84f1399c2a2770c505262ebef029934b78f36
Gustavo Felix <gustavo.felix2@portalsesisp.org.br>
2020-11-12 19:10:51 -0300

    Input e buttons inseridos

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const logar = () => {
+  alert(`${email} ${senha}`);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello my friends</Text>
+
+
+      <Text>Email</Text>
+      <TextInput
+      style={{ height: 40, width : 200, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => setEmail(text)}
+      value={email}
+    />
+
+
+    <Text>Senha</Text>
+    <TextInput
+      style={{ height: 40, color : 'white', width : 200, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => setSenha(text)}
+      value={senha}
+    />
+  <Button
+  onPress={logar}
+  title="Login"
+  color="green"
+  accessibilityLabel="Login"
+/>
       <StatusBar style="auto" />
     </View>
   );
-}
+
+  }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
